88060db414c1a061b1ade406cbb6938e65962355
vmaji <vmaji@DESKTOP-PEGQV09>
2019-09-14 22:52:19 +0100

    find me commit

---
+++ src/ApexPages.java
@@ -0,0 +1,20 @@
+
+public class ApexPages {
+
+	public class Message {
+
+	}
+
+	public static Object Severity;
+
+	public static Object currentPage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void addMessage(Message msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/PageReference.java
@@ -0,0 +1,9 @@
+
+public class PageReference {
+
+	public void setRedirect(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
